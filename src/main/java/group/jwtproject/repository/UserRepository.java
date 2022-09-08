e9aea6a9c368d79cd5a75d7e79434e8022a9fcbe
신정아 <jeonga@khu.ac.kr>
2022-09-08 22:25:56 +0900

    add : userRepository

---
+++ src/main/java/group/jwtproject/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package group.jwtproject.repository;
+
+import group.jwtproject.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    public User findByUsername(String username);
+}
